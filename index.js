bd4c3d873648298c0f9d57c94460c90f636536e4
Tara Smith <tarasmith@Taras-Air.lan>
2022-06-27 21:15:33 -0400

    added workable functions

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom){
+let newImage = document.createElement('img')
+newImage.src = url
+newImage.style.position = 'fixed'
+newImage.style.left = left + 'px'
+newImage.style.bottom = bottom + 'px'
+document.body.append(newImage)
+return(newImage)
+}
+newImage()
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+
+
+
+
+
+
+
+
+
