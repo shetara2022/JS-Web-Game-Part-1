b092e458dd65568c379c6ed619ae5dc888bfe368
Tara Smith <tarasmith@Taras-Air.lan>
2022-06-27 22:06:46 -0400

    final commit

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ return(newImage)
 }
 newImage()
 
+function newItem(url, left, bottom){
+  let newItem= document.createElement('img')
+newItem.src = url
+newItem.position = 'fixed'
+newItem.style.left = left + 'px'
+newItem.style.bottom = bottom + 'px'
+document.body.append(newItem)
+
+  newItem.addEventListener('dblclick', function(){
+      newItem.remove()
+  })
+  
+}
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
@@ -16,6 +29,10 @@ newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
 
 
 
